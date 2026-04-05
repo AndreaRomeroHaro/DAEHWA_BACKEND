df3f2b89473b101400e8ff8f3858f766fddf6202
AndreaRH <xaromhar009@ieshnosmachado.org>
2026-04-05 20:40:16 +0200

    correcion admin.py y configuracion CORS en settings

--- app_daehwa/admin.py
+++ app_daehwa/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import Usuario,Chat,Evaluacion_Inicial,Paciente,Diagnostico_Funcional,Plan_Intervencion,Cita,Registro_Sesiones
 
-admin.register(Usuario)
-admin.register(Chat)
-admin.register(Paciente)
-admin.register(Evaluacion_Inicial)
-admin.register(Diagnostico_Funcional)
-admin.register(Plan_Intervencion)
-admin.register(Cita)
-admin.register(Registro_Sesiones)
+admin.site.register(Usuario)
+admin.site.register(Chat)
+admin.site.register(Paciente)
+admin.site.register(Evaluacion_Inicial)
+admin.site.register(Diagnostico_Funcional)
+admin.site.register(Plan_Intervencion)
+admin.site.register(Cita)
+admin.site.register(Registro_Sesiones)
--- proyecto_daehwa/settings.py
+++ proyecto_daehwa/settings.py
@@ -130,6 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
+    "http://127.0.0.1:4200",
 ]
 
 MEDIA_URL = '/media/'
