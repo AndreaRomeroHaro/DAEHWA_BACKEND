c417f297c9c1de8c612ddd3dc84ceabbf0e92fc7
AndreaRH <xaromhar009@ieshnosmachado.org>
2026-04-05 20:19:40 +0200

    urls viewset

--- app_daehwa/urls.py
+++ app_daehwa/urls.py
@@ -1,5 +1,18 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import UsuarioViewSet,ChatViewSet,PacienteViewSet,Evaluacion_InicialViewSet,Diagnostico_FuncionalViewSet,Plan_IntervencionViewSet,CitaViewSet,Registro_SesionesViewSet
+
+router=DefaultRouter()
+
+router.register(r'usuarios',UsuarioViewSet)
+router.register(r'chats',ChatViewSet)
+router.register(r'pacientes',PacienteViewSet)
+router.register(r'evaluaciones-iniciales',Evaluacion_InicialViewSet)
+router.register(r'diagnosticos-funcionales',Diagnostico_FuncionalViewSet)
+router.register(r'planes-intervencion',Plan_IntervencionViewSet)
+router.register(r'citas',CitaViewSet)
+router.register(r'registro-sesiones',Registro_SesionesViewSet)
 
 urlpatterns=[
-    
+    path('',include(router.urls))
 ]
